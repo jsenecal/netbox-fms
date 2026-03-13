96ca7817416048b57090e7ed71ee54af80c4e400
Jonathan Senecal <jonathan.senecal@metrooptic.com>
2026-03-13 11:57:36 +0000

    feat: data migration to copy entrance_port.name to entrance_label (step 2/3)

---
+++ netbox_fms/migrations/0009_populate_entrance_label.py
@@ -0,0 +1,24 @@
+from django.db import migrations
+
+
+def copy_entrance_port_name(apps, schema_editor):
+    ClosureCableEntry = apps.get_model("netbox_fms", "ClosureCableEntry")
+    for entry in ClosureCableEntry.objects.select_related("entrance_port").filter(
+        entrance_port__isnull=False
+    ):
+        entry.entrance_label = entry.entrance_port.name
+        entry.save(update_fields=["entrance_label"])
+
+
+def noop(apps, schema_editor):
+    pass
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("netbox_fms", "0008_closurecableentry_entrance_label"),
+    ]
+
+    operations = [
+        migrations.RunPython(copy_entrance_port_name, noop),
+    ]
