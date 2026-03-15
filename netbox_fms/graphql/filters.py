e54bc4bf35018e1dc186875fd9e0f9fee049e144
Jonathan Senecal <jonathan.senecal@metrooptic.com>
2026-03-15 17:42:54 +0000

    feat: add meaningful filter fields to all GraphQL filter classes

--- netbox_fms/graphql/filters.py
+++ netbox_fms/graphql/filters.py
@@ -34,26 +34,36 @@ class FiberCableTypeFilter:
     fiber_type: str | None
     is_armored: bool | None
     deployment: str | None
+    strand_count: int | None
 
 
 @strawberry_django.filters.filter(BufferTubeTemplate)
 class BufferTubeTemplateFilter:
     id: int | None
+    fiber_cable_type_id: int | None
+    name: str | None
 
 
 @strawberry_django.filters.filter(RibbonTemplate)
 class RibbonTemplateFilter:
     id: int | None
+    fiber_cable_type_id: int | None
+    buffer_tube_template_id: int | None
 
 
 @strawberry_django.filters.filter(CableElementTemplate)
 class CableElementTemplateFilter:
     id: int | None
+    fiber_cable_type_id: int | None
+    element_type: str | None
 
 
 @strawberry_django.filters.filter(FiberCable)
 class FiberCableFilter:
     id: int | None
+    fiber_cable_type_id: int | None
+    cable_id: int | None
+    name: str | None
 
 
 @strawberry_django.filters.filter(SplicePlan)
@@ -61,24 +71,32 @@ class SplicePlanFilter:
     id: int | None
     name: str | None
     status: str | None
+    closure_id: int | None
+    project_id: int | None
 
 
 @strawberry_django.filters.filter(SplicePlanEntry)
 class SplicePlanEntryFilter:
     id: int | None
+    plan_id: int | None
+    tray_id: int | None
 
 
 @strawberry_django.filters.filter(SpliceProject)
 class SpliceProjectFilter:
     id: int | None
+    name: str | None
 
 
 @strawberry_django.filters.filter(ClosureCableEntry)
 class ClosureCableEntryFilter:
     id: int | None
+    closure_id: int | None
+    fiber_cable_id: int | None
 
 
 @strawberry_django.filters.filter(FiberPathLoss)
 class FiberPathLossFilter:
     id: int | None
+    cable_id: int | None
     wavelength_nm: int | None
