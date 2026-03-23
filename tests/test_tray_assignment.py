e69267d9c0b410231c1d51876459be1f8c2690bd
Jonathan Senecal <jonathan.senecal@metrooptic.com>
2026-03-23 04:22:37 +0000

    test: add API coverage tests for TrayProfile and TubeAssignment
    Add TestTrayProfileAPI and TestTubeAssignmentAPI to test_tray_assignment.py,
    covering create (POST 201) and list (GET 200) endpoints using force_authenticate.

--- tests/test_tray_assignment.py
+++ tests/test_tray_assignment.py
@@ -1,11 +1,14 @@
 """Tests for TrayProfile and TubeAssignment models."""
 
-from django.test import TestCase
+from dcim.models import Device, DeviceRole, DeviceType, Manufacturer, Module, ModuleType, Site
+from django.contrib.auth import get_user_model
 from django.db import IntegrityError
-from dcim.models import Device, DeviceRole, DeviceType, Manufacturer, ModuleType, Module, Site
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
 
 from netbox_fms.choices import TrayRoleChoices
-from netbox_fms.models import TrayProfile, TubeAssignment, FiberCableType, FiberCable, BufferTube, ClosureCableEntry
+from netbox_fms.models import BufferTube, ClosureCableEntry, FiberCable, FiberCableType, TrayProfile, TubeAssignment
 
 
 class TestTrayProfile(TestCase):
@@ -94,10 +97,14 @@ class TestTubeAssignment(TestCase):
 
         # Fiber cable with buffer tube
         fct = FiberCableType.objects.create(
-            manufacturer=manufacturer, model="12F Cable", construction="loose_tube",
-            fiber_type="smf_os2", strand_count=12,
+            manufacturer=manufacturer,
+            model="12F Cable",
+            construction="loose_tube",
+            fiber_type="smf_os2",
+            strand_count=12,
         )
         from dcim.models import Cable
+
         cable = Cable.objects.create()
         cls.fiber_cable = FiberCable.objects.create(cable=cable, fiber_cable_type=fct)
         cls.tube = BufferTube.objects.create(fiber_cable=cls.fiber_cable, name="Tube 1", position=1)
@@ -109,13 +116,16 @@ class TestTubeAssignment(TestCase):
 
     def test_create_tube_assignment(self):
         ta = TubeAssignment.objects.create(
-            closure=self.closure, tray=self.tray, buffer_tube=self.tube,
+            closure=self.closure,
+            tray=self.tray,
+            buffer_tube=self.tube,
         )
         assert ta.pk is not None
 
     def test_unique_tube_per_closure(self):
         TubeAssignment.objects.create(closure=self.closure, tray=self.tray, buffer_tube=self.tube)
         from dcim.models import ModuleBay
+
         bay_extra = ModuleBay.objects.create(device=self.closure, name="Bay Extra")
         tray2 = Module.objects.create(device=self.closure, module_bay=bay_extra, module_type=self.module_type)
         with self.assertRaises(IntegrityError):
@@ -130,14 +140,17 @@ class TestTubeAssignment(TestCase):
         assert "/tube-assignments/" in ta.get_absolute_url()
 
     def test_clean_tray_must_belong_to_closure(self):
-        from django.core.exceptions import ValidationError
         from dcim.models import ModuleBay
+        from django.core.exceptions import ValidationError
+
         other_site = Site.objects.create(name="Other Site", slug="other-site")
         other_dt = DeviceType.objects.create(
             manufacturer=Manufacturer.objects.first(), model="Other DT", slug="other-dt"
         )
         other_device = Device.objects.create(
-            name="Other", site=other_site, device_type=other_dt,
+            name="Other",
+            site=other_site,
+            device_type=other_dt,
             role=DeviceRole.objects.first(),
         )
         other_bay = ModuleBay.objects.create(device=other_device, name="Other Bay")
@@ -148,23 +161,30 @@ class TestTubeAssignment(TestCase):
 
     def test_clean_tray_must_have_profile(self):
         from django.core.exceptions import ValidationError
+
         ta = TubeAssignment(closure=self.closure, tray=self.plain_module, buffer_tube=self.tube)
         with self.assertRaises(ValidationError):
             ta.full_clean()
 
     def test_clean_tray_must_be_splice_tray(self):
         from django.core.exceptions import ValidationError
+
         ta = TubeAssignment(closure=self.closure, tray=self.express_module, buffer_tube=self.tube)
         with self.assertRaises(ValidationError):
             ta.full_clean()
 
     def test_clean_cable_entry_must_exist(self):
         from django.core.exceptions import ValidationError
+
         fct2 = FiberCableType.objects.create(
-            manufacturer=Manufacturer.objects.first(), model="Other Cable",
-            construction="loose_tube", fiber_type="smf_os2", strand_count=12,
+            manufacturer=Manufacturer.objects.first(),
+            model="Other Cable",
+            construction="loose_tube",
+            fiber_type="smf_os2",
+            strand_count=12,
         )
         from dcim.models import Cable
+
         cable2 = Cable.objects.create()
         fc2 = FiberCable.objects.create(cable=cable2, fiber_cable_type=fct2)
         tube2 = BufferTube.objects.create(fiber_cable=fc2, name="Tube X", position=1)
@@ -185,16 +205,21 @@ class TestClosureCableEntryCascade(TestCase):
         cls.closure = Device.objects.create(name="Closure-C", site=site, device_type=device_type, role=role)
 
         from dcim.models import ModuleBay
+
         module_type = ModuleType.objects.create(manufacturer=manufacturer, model="Tray C")
         TrayProfile.objects.create(module_type=module_type, tray_role=TrayRoleChoices.SPLICE_TRAY)
         bay = ModuleBay.objects.create(device=cls.closure, name="Bay C")
         cls.tray = Module.objects.create(device=cls.closure, module_bay=bay, module_type=module_type)
 
         fct = FiberCableType.objects.create(
-            manufacturer=manufacturer, model="Cable C", construction="loose_tube",
-            fiber_type="smf_os2", strand_count=12,
+            manufacturer=manufacturer,
+            model="Cable C",
+            construction="loose_tube",
+            fiber_type="smf_os2",
+            strand_count=12,
         )
         from dcim.models import Cable
+
         cable = Cable.objects.create()
         cls.fiber_cable = FiberCable.objects.create(cable=cable, fiber_cable_type=fct)
         cls.tube = BufferTube.objects.create(fiber_cable=cls.fiber_cable, name="Tube C1", position=1)
@@ -208,3 +233,84 @@ class TestClosureCableEntryCascade(TestCase):
 
         entry.delete()
         assert not TubeAssignment.objects.filter(closure=self.closure, buffer_tube=self.tube).exists()
+
+
+class TestTrayProfileAPI(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user_model = get_user_model()
+        cls.user = user_model.objects.create_superuser("apiuser_tp", "api_tp@test.com", "testpass")
+        manufacturer = Manufacturer.objects.create(name="API Mfr TP", slug="api-mfr-tp")
+        cls.module_type = ModuleType.objects.create(manufacturer=manufacturer, model="API Tray")
+
+    def setUp(self):
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_create_tray_profile(self):
+        url = reverse("plugins-api:netbox_fms-api:trayprofile-list")
+        data = {"module_type": self.module_type.pk, "tray_role": "splice_tray"}
+        response = self.client.post(url, data, format="json")
+        assert response.status_code == 201
+
+    def test_list_tray_profiles(self):
+        TrayProfile.objects.create(module_type=self.module_type, tray_role=TrayRoleChoices.SPLICE_TRAY)
+        url = reverse("plugins-api:netbox_fms-api:trayprofile-list")
+        response = self.client.get(url)
+        assert response.status_code == 200
+        assert response.json()["count"] == 1
+
+
+class TestTubeAssignmentAPI(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user_model = get_user_model()
+        cls.user = user_model.objects.create_superuser("apiuser_ta", "api_ta@test.com", "testpass")
+
+        site = Site.objects.create(name="API TA Site", slug="api-ta-site")
+        manufacturer = Manufacturer.objects.create(name="API TA Mfr", slug="api-ta-mfr")
+        device_type = DeviceType.objects.create(manufacturer=manufacturer, model="API Closure", slug="api-closure")
+        role = DeviceRole.objects.create(name="API Closure", slug="api-closure")
+        cls.closure = Device.objects.create(name="API-Closure", site=site, device_type=device_type, role=role)
+
+        from dcim.models import ModuleBay
+
+        module_type = ModuleType.objects.create(manufacturer=manufacturer, model="API Tray TA")
+        TrayProfile.objects.create(module_type=module_type, tray_role=TrayRoleChoices.SPLICE_TRAY)
+        bay = ModuleBay.objects.create(device=cls.closure, name="API Bay")
+        cls.tray = Module.objects.create(device=cls.closure, module_bay=bay, module_type=module_type)
+
+        fct = FiberCableType.objects.create(
+            manufacturer=manufacturer,
+            model="API Cable",
+            construction="loose_tube",
+            fiber_type="smf_os2",
+            strand_count=12,
+        )
+        from dcim.models import Cable
+
+        cable = Cable.objects.create()
+        cls.fiber_cable = FiberCable.objects.create(cable=cable, fiber_cable_type=fct)
+        cls.tube = BufferTube.objects.create(fiber_cable=cls.fiber_cable, name="API Tube", position=1)
+        ClosureCableEntry.objects.create(closure=cls.closure, fiber_cable=cls.fiber_cable, entrance_label="G1")
+
+    def setUp(self):
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_create_tube_assignment(self):
+        url = reverse("plugins-api:netbox_fms-api:tubeassignment-list")
+        data = {
+            "closure": self.closure.pk,
+            "tray": self.tray.pk,
+            "buffer_tube": self.tube.pk,
+        }
+        response = self.client.post(url, data, format="json")
+        assert response.status_code == 201
+
+    def test_list_tube_assignments(self):
+        TubeAssignment.objects.create(closure=self.closure, tray=self.tray, buffer_tube=self.tube)
+        url = reverse("plugins-api:netbox_fms-api:tubeassignment-list")
+        response = self.client.get(url)
+        assert response.status_code == 200
+        assert response.json()["count"] == 1
